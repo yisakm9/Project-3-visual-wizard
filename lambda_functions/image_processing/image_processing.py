c43f11e86fc76055b4e0eedae7c80d6ff3f755cb
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-08-20 18:40:15 +0300

    Add URL decoding for S3 object key in image processing Lambda function

--- lambda_functions/image_processing/image_processing.py
+++ lambda_functions/image_processing/image_processing.py
@@ -1,14 +1,20 @@
 import boto3
 import os
+import urllib.parse  
 
 s3_client = boto3.client('s3')
-rekognition_client = boto3.client('rekognition')
+rekognition_client = boto.client('rekognition')
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
+    
+    # Original key from the event
     key = event['Records'][0]['s3']['object']['key']
+    
+    # <-- 2. ADD THIS LINE TO DECODE THE KEY
+    key = urllib.parse.unquote_plus(key)
 
     response = rekognition_client.detect_labels(
         Image={
