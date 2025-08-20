b4afe20016b35ab433000ac04be167f3e2975105
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-08-20 07:05:16 +0300

    Enable encryption and DynamoDB locking for S3 backend in main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ terraform {
     bucket       = "ysak-terraform-state-bucket"
     key          = "voicevault/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = true
+    encrypt      = true
+    dynamodb_table = true
   }
 }
   terraform {
