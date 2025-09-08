e9d6ad699cbb2046963796c8d965cce8b62702b8
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-09-08 18:20:42 +0300

    feat: Add required_version attribute to Terraform configuration in kms module

---
+++ modules/kms/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  # ADD THIS required_version ATTRIBUTE
+  required_version = "~>  1.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
