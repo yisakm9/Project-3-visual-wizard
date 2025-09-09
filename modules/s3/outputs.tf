6c4d9b976991bf1708a3da7d87a487f5d90cb191
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-09-09 20:09:21 +0300

    d

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -7,3 +7,8 @@ output "bucket_arn" {
   description = "The ARN of the S3 bucket."
   value       = aws_s3_bucket.this.arn
 }
+
+output "bucket_id" {
+  description = "The ID (name) of the S3 bucket."
+  value       = aws_s3_bucket.this.id
+}
