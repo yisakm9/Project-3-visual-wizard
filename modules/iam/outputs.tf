3ba3191ff57cdf7a17a30774f7abe0daa71479fc
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-09-10 05:04:00 +0300

    iam fix

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -2,3 +2,8 @@ output "role_arn" {
   description = "The ARN of the created IAM role."
   value       = aws_iam_role.this.arn
 }
+
+output "role_name" {
+  description = "The name of the created IAM role."
+  value       = aws_iam_role.this.name
+}
