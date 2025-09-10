5a146ad352ac117ec43624386fbd22f00b25bf52
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-09-10 08:12:51 +0300

    fix(s3): Use kms_key_arn variable for bucket encryption

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -17,6 +17,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     apply_server_side_encryption_by_default {
        sse_algorithm     = "aws:kms"
        # sse_algorithm = "AES256"
+       kms_master_key_id = var.kms_key_arn
     }
   }
 }
